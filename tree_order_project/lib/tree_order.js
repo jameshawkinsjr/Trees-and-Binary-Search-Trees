ea1b56aa205bd757e7b7ff5fe52b0ba4af0d5c47
James <james@jameshawkins.com>
2019-03-20 15:22:42 -0700

    Tree order project

--- tree_order_project/lib/tree_order.js
+++ tree_order_project/lib/tree_order.js
@@ -1,8 +1,13 @@
 function inOrderArray(root) {
+    if (!root) return [];
 
+    return inOrderArray(root.left).concat(root.val).concat(inOrderArray(root.right))
 }
 
 function postOrderArray(root) {
+    if (!root) return [];
+
+    return postOrderArray(root.left).concat(postOrderArray(root.right)).concat(root.val)
 
 }
 
